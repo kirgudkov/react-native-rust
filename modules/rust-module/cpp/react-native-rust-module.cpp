22f4c35504fa8196e533446195da62a1d25895b9
Kirill Gudkov <for.example.nickname@gmail.com>
2024-10-21 12:55:48 +0400

    fixed cxxBridge.runtime missing after reload refactored code

--- modules/rust-module/cpp/react-native-rust-module.cpp
+++ modules/rust-module/cpp/react-native-rust-module.cpp
@@ -7,31 +7,25 @@ using namespace facebook;
 using namespace std;
 
 void install(jsi::Runtime &jsiRuntime) {
-    auto generateQR = jsi::Function::createFromHostFunction(jsiRuntime, jsi::PropNameID::forAscii(jsiRuntime, "generateQR"), 1, [](jsi::Runtime &runtime, const jsi::Value &thisValue, const jsi::Value *arguments, size_t count) -> jsi::Value {
-		string data = arguments[0].getString(runtime).utf8(runtime);
-		char *rust_svg_string = rust::generate_qr(data.c_str());
-		string cpp_svg_string(rust_svg_string);
-		rust::free_string(rust_svg_string);
+	// use std::move() if the result of jsi::Function::createFromHostFunction() is stored in a variable
+	jsiRuntime.global().setProperty(jsiRuntime, "multiply", jsi::Function::createFromHostFunction(jsiRuntime, jsi::PropNameID::forAscii(jsiRuntime, "multiply"), 2, [](jsi::Runtime &runtime, const jsi::Value &thisValue, const jsi::Value *arguments, size_t count) -> jsi::Value {
+		// {} is a shorthand for jsi::Value(double) constructor
+		return {rust::multiply(arguments[0].getNumber(), arguments[1].getNumber())};
+	}));
+	
+	jsiRuntime.global().setProperty(jsiRuntime, "sayHello", jsi::Function::createFromHostFunction(jsiRuntime, jsi::PropNameID::forAscii(jsiRuntime, "say_hello"), 0, [](jsi::Runtime &runtime, const jsi::Value &thisValue, const jsi::Value *arguments, size_t count) -> jsi::Value {
+		char *rustStr = rust::say_hello();
+		string cppStr(rustStr);
+		rust::free_string(rustStr);
 		
-		return jsi::String::createFromUtf8(runtime, cpp_svg_string);
-    });
-
-    jsiRuntime.global().setProperty(jsiRuntime, "generateQR", std::move(generateQR));
+		return jsi::String::createFromUtf8(runtime, cppStr);
+	}));
 	
-	
-	auto multiply = jsi::Function::createFromHostFunction(jsiRuntime, jsi::PropNameID::forAscii(jsiRuntime, "multiply"), 2, [](jsi::Runtime &runtime, const jsi::Value &thisValue, const jsi::Value *arguments, size_t count) -> jsi::Value {
-        return {rust::multiply(arguments[0].getNumber(), arguments[1].getNumber())};
-    });
-
-    jsiRuntime.global().setProperty(jsiRuntime, "multiply", std::move(multiply));
-
-    auto sayHello = jsi::Function::createFromHostFunction(jsiRuntime, jsi::PropNameID::forAscii(jsiRuntime, "say_hello"), 0, [](jsi::Runtime &runtime, const jsi::Value &thisValue, const jsi::Value *arguments, size_t count) -> jsi::Value {
-        char *rust_string = rust::say_hello();
-        string cpp_string(rust_string);
-        rust::free_string(rust_string);
-
-        return jsi::String::createFromUtf8(runtime, cpp_string);
-    });
-
-    jsiRuntime.global().setProperty(jsiRuntime, "sayHello", std::move(sayHello));
+	jsiRuntime.global().setProperty(jsiRuntime, "generateQR", jsi::Function::createFromHostFunction(jsiRuntime, jsi::PropNameID::forAscii(jsiRuntime, "generateQR"), 1, [](jsi::Runtime &runtime, const jsi::Value &thisValue, const jsi::Value *arguments, size_t count) -> jsi::Value {
+		char *rustSvgStr = rust::generate_qr(arguments[0].getString(runtime).utf8(runtime).c_str());
+		string cppSvgStr(rustSvgStr);
+		rust::free_string(rustSvgStr);
+		
+		return jsi::String::createFromUtf8(runtime, cppSvgStr);
+	}));
 }
--- modules/rust-module/ios/RustModule.mm
+++ modules/rust-module/ios/RustModule.mm
@@ -6,27 +6,28 @@
 @implementation RustModule
 
 @synthesize bridge = _bridge;
-@synthesize methodQueue = _methodQueue;
 
+// This macro is used to register the module with React Native.
+// It exposes this Objective-C++ class as a native module that JavaScript can interact with.
+// The module name is automatically inferred from the class name, but we can pass a custom name as an argument if needed.
 RCT_EXPORT_MODULE()
 
 + (BOOL)requiresMainQueueSetup {
-  return YES;
+	return YES;
 }
 
 - (void)setBridge:(RCTBridge *)bridge
 {
-  _bridge = bridge;
-  _setBridgeOnMainQueue = RCTIsMainQueue();
-
-  RCTCxxBridge *cxxBridge = (RCTCxxBridge *)self.bridge;
-  if (!cxxBridge.runtime) {
-	return;
-  }
-
-  [bridge dispatchBlock:^{
-	install(*(facebook::jsi::Runtime *)cxxBridge.runtime);
-  } queue:RCTJSThread];
+	_bridge = bridge;
+	
+	[self.bridge dispatchBlock:^{
+		RCTCxxBridge *cxxBridge = (RCTCxxBridge *)self.bridge;
+		if (!cxxBridge.runtime) {
+			return;
+		}
+		
+		install(*(facebook::jsi::Runtime *)cxxBridge.runtime);
+	} queue:RCTJSThread];
 }
 
 @end
